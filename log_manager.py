7f0dc98fe72dbfb6fc945afd97535d28783fa31b
ShamNeko <mkdvlp01@gmail.com>
2024-06-15 23:11:08 +0900

    ドキュメンテーションコメントの変更

--- log_manager.py
+++ log_manager.py
@@ -1,11 +1,11 @@
 """
-Summaly:
+- Summaly:
     - logのファイル出力等の操作を簡易的に行う
 
-Example:
+- Example:
     - log.txt及びコンソールにログを出力
 
-    ```
+    ```Python
     import log_manager
     log = log_manager.LogHandler(
         __name__, "log.txt", log_level=10, debug_mode=True
