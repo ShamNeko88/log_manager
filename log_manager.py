a521d7755e17dceff226e125f6ba6a330457e6c4
ShamNeko <mkdvlp01@gmail.com>
2024-06-17 19:19:07 +0900

    ドキュメンテーションコメント変更

--- log_manager.py
+++ log_manager.py
@@ -1,18 +1,15 @@
 """
-- Summaly:
-    - logのファイル出力等の操作を簡易的に行う
+logのファイル出力等の操作を簡易的に行う
 
-- Example:
-    - log.txt及びコンソールにログを出力
+Example:
+    log.txt及びコンソールにログを出力
 
-    ```Python
-    import log_manager
-    log = log_manager.LogHandler(
-        __name__, "log.txt", log_level=10, debug_mode=True
-    )
-    log.logger.debug("test")
-    > [DEBUG] date time -test <場所: "ファイル名.py"/"module名"/xx行目>
-    ```
+    >>>import log_manager
+    >>>log = log_manager.LogHandler(
+    ...    __name__, "log.txt", log_level=10, debug_mode=True
+    ...)
+    >>>log.logger.debug("test")
+    [DEBUG] date time -test <場所: "ファイル名.py"/"module名"/xx行目>
 """
 import logging
 from logging.handlers import RotatingFileHandler as r_handler
