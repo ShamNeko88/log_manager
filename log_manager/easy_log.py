50ced1cd48c18e52dc7f262f16a815a8d90b1e7a
ShamNeko <mkdvlp01@gmail.com>
2024-09-12 23:53:09 +0900

    [WIP]loggerを取得返すメソッド作成

--- log_manager/easy_log.py
+++ log_manager/easy_log.py
@@ -5,10 +5,11 @@ Example:
     log.txt及びコンソールにログを出力
 
     >>>import log_manager
-    >>>log = log_manager.LogHandler(
+    >>>log_ins = log_manager.LogHandler(
     ...    __name__, "log.txt", log_level=10, debug_mode=True
     ...)
-    >>>log.logger.debug("test")
+    >>>log = log_ins.get_logger()
+    >>>log.debug("test")
     [DEBUG] date time -test <場所: "ファイル名.py"/"module名"/xx行目>
 """
 import logging
@@ -26,29 +27,41 @@ class LogHandler():
         log_level:     デフォルトですべて出力(CRITICAL:50, ERROR:40, WARNING:30, INFO:20, DEBUG:10, NOTSET:0)
         debug_mode:    True明示指定でコンソールに出力
         max_bytes:     ログファイルの最大バイト数
-        backup_count:   最大バイト超えたときに保持しておくファイル数
+        backup_count:  最大バイト超えたときに保持しておくファイル数
         encoding:      文字コード指定
+
+    Methods:
+        get_logger:    loggerの取得
     """
-    def __init__(self, logger_name: str,
-                 log_file: str = None,
-                 log_level: int = 10,
-                 debug_mode=False,
-                 max_bytes: int = 10000,
-                 backup_count: int = 2,
-                 encoding: str = "utf-8"):
+    def __init__(
+        self,
+        logger_name: str = "__name__",
+        log_file: str = None,
+        log_level: int = 10,
+        debug_mode=False,
+        max_bytes: int = 10000,
+        backup_count: int = 2,
+        encoding: str = "utf-8"
+    ):
         # フォーマット（ファイル）
-        self.log_file_format = logging.Formatter("[%(levelname)s] %(asctime)s -%(message)s <場所: %(filename)s/%(funcName)s/%(lineno)d行目>")
+        self.log_file_format = logging.Formatter(
+            "[%(levelname)s] %(asctime)s -%(message)s <場所: %(filename)s/%(funcName)s/%(lineno)d行目>"
+        )
         # フォーマット（コンソール）
-        self.log_console_format = logging.Formatter("[%(levelname)s] %(asctime)s -%(message)s <場所: %(filename)s/%(funcName)s/%(lineno)d行目>")
+        self.log_console_format = logging.Formatter(
+            "[%(levelname)s] %(asctime)s -%(message)s <場所: %(filename)s/%(funcName)s/%(lineno)d行目>"
+        )
         # loggerの作成
         self.logger = logging.getLogger(logger_name)
         # logレベル設定
         self.logger.setLevel(log_level)
         # logファイル出力先とファイルの最大バイト数、ファイル数設定
         if log_file:
-            file_handler = r_handler(log_file, maxBytes=max_bytes,
-                                     backupCount=backup_count,
-                                     encoding=encoding)
+            file_handler = r_handler(
+                log_file, maxBytes=max_bytes,
+                backupCount=backup_count,
+                encoding=encoding
+            )
             file_handler.setFormatter(self.log_file_format)
             self.logger.addHandler(file_handler)
 
@@ -59,10 +72,5 @@ class LogHandler():
             # 設定したハンドラーセット
             self.logger.addHandler(console_handler)
 
-
-if __name__ == "__main__":
-    import log_manager
-    log = log_manager.LogHandler(
-        __name__, "log.txt", log_level=10, debug_mode=True
-    )
-    log.logger.debug("test")
+    def get_logger(self):
+        return self.logger
